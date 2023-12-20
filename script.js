67914ecbdfdab0c1bc067ddabe8ab9490fb1af2f
unknown <anshbhanushali63690@gmail.com>
2023-12-19 20:11:23 -0500

    My commits

---
+++ script.js
@@ -0,0 +1,44 @@
+let menuIcon = document.querySelector('#menu-icon'); // Corrected variable name to 'menuIcon'
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section'); // Changed variable name to 'sections'
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active'); // Changed 'navLinks' to 'link' here
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    if (menuIcon.classList.contains('bx-x')) {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    }
+};
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
